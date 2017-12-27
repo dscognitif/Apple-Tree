3f863882db050be48b9983deaf33c32cc97d4571
Ronin Winter <32187231+dscognitif@users.noreply.github.com>
2017-12-27 15:09:40 +0700

    Add files via upload

---
+++ Words/Game.swift
@@ -0,0 +1,40 @@
+//
+//  Game.swift
+//  Words
+//
+//  Created by Ronin Winter on 12/2/17.
+//  Copyright © 2017 Ronin Winter. All rights reserved.
+//
+
+import Foundation
+
+struct Game {
+    
+    var word: String
+    var incorrectMovesRemaining: Int
+    var guessedLetters: [Character]
+    
+    mutating func playerGuessed(letter: Character) {
+        guessedLetters.append(letter)
+        if !word.characters.contains(letter) {
+            incorrectMovesRemaining -= 1
+        }
+    }
+    
+    var formattedWord: String {
+        var guessedWord = ""
+        for letter in word.characters {
+            if guessedLetters.contains((letter)) {
+                guessedWord += "\(letter)"
+            } else {
+                guessedWord += "_"
+            }
+        }
+        return guessedWord
+    }
+    
+}
+
+
+
+
